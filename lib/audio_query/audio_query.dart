02587e1b42ab6e41ca3c463363e2957bef31c5cc
PNT <92286791+PNThanggg@users.noreply.github.com>
2023-12-05 10:46:24 +0700

    :page_facing_up: flutter audio query

---
+++ lib/audio_query/audio_query.dart
@@ -0,0 +1,18 @@
+library audio_query;
+
+// Dart
+import 'dart:async';
+
+// Flutter
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+// Platform Interface
+import '../audio_query_platform_interface/audio_query_platform_interface.dart';
+import '../audio_query_platform_interface/src/audio_query_core.dart';
+
+// Controllers
+part 'src/audio_query.dart';
+
+// Widgets
+part 'widget/query_artwork_widget.dart';
